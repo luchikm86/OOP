2606957a3b5fb926959348201173e8c31f7a6b89
luchikm86 <35977108+luchikm86@users.noreply.github.com>
2018-03-06 15:43:24 +0200

    Update SearchSystemFilms.java

--- kurs/project/SearchSystemFilms.java
+++ kurs/project/SearchSystemFilms.java
@@ -2,23 +2,20 @@ public class SearchSystemFilms {	//	поисковая система фильм
 	/*
 		Описание сущщностей которые присутствуют в системе.
 	*/
-	privat final String name;	//	имя
-	privat final String author;	//	автор
-	privat final String user;	//	пользователь системы
-	privat final String description;	// описание
-	privat final int years;	//	год
-	privat final String country;	//	страна
-	privat final long like;	//	лайк
-	privat final long dislike;	// дизлайк
-	privat final double	rating; //	рейтинг
-	privat final String reviews;	//	отзывы
-	privat final int	durability;	//длительность фильма в минутах
-	privat final ArrayList <FavoriteMovies> favoritesMovies = new ArrayList();	//	список избранных фильмов
-	privat final ArrayList <FavoriteAuthor> favoriteAuthor = new ArrayList();	// список любимых актеров
-	privat final ArrayList <WatchedMovies> watchedMovies = new ArrayList();	// список просмотренных фильмов с рейтингом
-	
-	
-	
+	private final String name;	//	имя
+	private final String author;	//	автор
+	private final String user;	//	пользователь системы
+	private final String description;	// описание
+	private final int years;	//	год
+	private final String country;	//	страна
+	private final long like;	//	лайк
+	private final long dislike;	// дизлайк
+	private final double rating; //	рейтинг
+	private final String reviews;	//	отзывы
+	private final int	durability;	//длительность фильма в минутах
+	private final ArrayList <FavoriteMovies> favoritesMovies = new ArrayList();	//	список избранных фильмов
+	private final ArrayList <FavoriteAuthor> favoriteAuthor = new ArrayList();	// список любимых актеров
+	private final ArrayList <WatchedMovies> watchedMovies = new ArrayList();	// список просмотренных фильмов с рейтингом
 	
 	/*
 		Описание связи между сущностями.
@@ -28,8 +25,20 @@ public class SearchSystemFilms {	//	поисковая система фильм
 		Пользователь может хранить список любимых актеров.
 		Список просмотренных фильмов с рейтингом
 	*/
-	
-	
+	public SearchSystemFilms (String name, String author, String user, String description, 
+				  int years, String country, long like, long dislike, double rating, String reviews, int durability) {
+		this.name = name;
+		this.author = author;
+		this.user = user;
+		this.description = description;
+		this.years = years;
+		this.country = country;
+		this.loke = like;
+		this.dislike = dislike;
+		this.rating = rating;
+		this.reviews = reviews;
+		this.durability = durability;
+	}
 	
 	
 
