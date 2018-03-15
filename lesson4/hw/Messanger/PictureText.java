9da3be40e972c67c35c5260e9376e1ff3fe08bc2
luchikm86 <35977108+luchikm86@users.noreply.github.com>
2018-03-15 13:21:18 +0200

    Update PictureText.java

--- lesson4/hw/Messanger/PictureText.java
+++ lesson4/hw/Messanger/PictureText.java
@@ -1,4 +1,21 @@
 package com.mluch.oop.lesson4.hw.Messanger;
 
 public class PictureText {
+  
+  private finaly String picture;
+  
+  public PictureText (String picture) {
+   this.picture = picture; 
+  }
+  
+  public String getPicture () {
+   return picture; 
+  }
+  
+  public String toString() {
+   return "PictureText {" +
+     "picture= " + picture +
+     "}";
+  }
+  
 }
