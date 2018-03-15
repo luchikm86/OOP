544d487cfc8e3eaa6f82507dcf82621e030d826f
luchikm86 <35977108+luchikm86@users.noreply.github.com>
2018-03-15 13:03:39 +0200

    Update EmoticonText.java

--- lesson4/hw/Messanger/EmoticonText.java
+++ lesson4/hw/Messanger/EmoticonText.java
@@ -1,4 +1,20 @@
 package com.mluch.oop.lesson4.hw.Messanger;
 
 public class EmoticonText {
+  // по стрингу можно сделать интерфейс и где есть стринг то нужно добавить в обьект ингтерфес
+  private finaly String emoticon;
+  
+  public EmoticonText (String emoticon) {
+   this.emoticon =  emoticon;
+  }
+  
+  public string getEmoticon () {
+   return emoticon; 
+  }
+  
+  public String toString () {
+   return "EmoticonText {" +
+     "emoticon= " + emoticon +
+     "}"; 
+  }
 }
