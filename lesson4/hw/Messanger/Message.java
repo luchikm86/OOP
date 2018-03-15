acf18ec3e117c2db25bdf7fe46263c1b22e4901b
luchikm86 <35977108+luchikm86@users.noreply.github.com>
2018-03-15 12:23:31 +0200

    Update Message.java

--- lesson4/hw/Messanger/Message.java
+++ lesson4/hw/Messanger/Message.java
@@ -1,5 +1,35 @@
 package com.mluch.oop.lesson4.hw.Messanger;
 
 public class Message {
+  
+  private finaly String author;
+  private finaly LocalDate locDate;
+  private finaly String countent;
+  
+  public Message (String author, LocalDate locDate, String countent) {
+    this.author = author;
+    this.locDate = locDate;
+    this.countent = countent;
+  }
+  
+  public String getAuthor() {
+    return author;
+  }
+  
+  public LocalDate locDate() {
+    return locDate;
+  }
+  
+  public String getCountent() {
+    return countent;
+  }
+  
+  public String toString() {
+   return "Message{" + 
+     "author= " + author +
+     "locDate= " + locDate +
+     "countent= " + countent +
+     '}';
+  }
 
 }
