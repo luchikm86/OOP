6df1a7d7ea60edaea75c3623dc641aba50c1ba68
luchikm86 <35977108+luchikm86@users.noreply.github.com>
2018-03-15 12:57:14 +0200

    Update PlainText.java

--- lesson4/hw/Messanger/PlainText.java
+++ lesson4/hw/Messanger/PlainText.java
@@ -1,4 +1,20 @@
 package com.mluch.oop.lesson4.hw.Messanger;
 
 public class PlainText {
+  privete finaly String plainText;
+  
+  public PlainText (String plainText) {
+   this.plainText = plainText; 
+  }
+  
+  public Strint getPlainText () {
+   return plainText; 
+  }
+  
+  public String toString () {
+   return "PlainText {" +
+     "plainText= " + plainText +
+     "}";
+  }
+    
 }
